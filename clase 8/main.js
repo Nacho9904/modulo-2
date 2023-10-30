a8862d0966fed81b98d6addf3a6f2827b2c80405
Ignacio Barrera Oro <ignaciobarreraoro@gmail.com>
2023-10-30 16:19:41 -0300

    Clase-8 JS

--- clase 8/main.js
+++ clase 8/main.js
@@ -1,10 +1,30 @@
-let nombre = prompt ('Ingrese Su Nombre')
+/* let nombre = prompt ('Ingrese Su Nombre')
 let edad = Number (prompt ('Ingrese Su Edad'))
 let actividad = prompt ('Ingrese Su actividad Favorita')
 let deporte = prompt ('Ingrese su Deporte Favorito')
 
-let tarjeta = 'Hola mi nombre es ' + nombre + ', tengo '
- + edad + ' años, Mi actividad favorita es ' + Actividad
-+ 'Y mi deporte favorito es' + deporte;
+let tarjeta = ' Hola mi nombre es ' + nombre + ', tengo '
+ + edad + ' años, Mi actividad favorita es ' + actividad
++ ' Y mi deporte favorito es ' + deporte;
 
-alert (tarjeta)
+alert (tarjeta) */
+
+
+/* let error1 = 'El dijo: Hola mundo'
+let error2 = 'Im Ignacio' */
+
+let solucion1 = 'El dijo: Hola mundo'
+let solucion2 = 'Im Ignacio'
+
+
+let s = 'Hola zapatilla'
+console.log (s)
+console.log(s.toUpperCase())
+console.log(s.toLowerCase())
+console.log(s.charAt(2))
+console.log (s.indexOf('0'))
+console.log (s.lastIndexOf('0'))
+console.log (s.slice(0,5))
+console.log (s.split('a,o'))
+console.log (s.concat('s'))
+console.log (s.length)
