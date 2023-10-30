c345d41fe63d396e22985f250793c4c683b87291
Ignacio Barrera Oro <ignaciobarreraoro@gmail.com>
2023-10-30 16:16:23 -0300

    clase-8

---
+++ clase 8/main.js
@@ -0,0 +1,10 @@
+let nombre = prompt ('Ingrese Su Nombre')
+let edad = Number (prompt ('Ingrese Su Edad'))
+let actividad = prompt ('Ingrese Su actividad Favorita')
+let deporte = prompt ('Ingrese su Deporte Favorito')
+
+let tarjeta = 'Hola mi nombre es ' + nombre + ', tengo '
+ + edad + ' años, Mi actividad favorita es ' + Actividad
++ 'Y mi deporte favorito es' + deporte;
+
+alert (tarjeta)
